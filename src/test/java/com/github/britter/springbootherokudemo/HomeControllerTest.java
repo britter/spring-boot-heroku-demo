c335bcaa3a9ebfac5fca6fc1c3ebeb32c91f2d5a
Benedikt Ritter <beneritter@gmail.com>
2016-01-09 16:26:23 +0100

    Break test to show how CI saves us

--- src/test/java/com/github/britter/springbootherokudemo/HomeControllerTest.java
+++ src/test/java/com/github/britter/springbootherokudemo/HomeControllerTest.java
@@ -62,7 +62,7 @@ public class HomeControllerTest {
         public void shouldAddInsertRecordToModelMap() throws Exception {
             ctrl.home(map);
 
-            assertThat(map, hasKey("insertRecord"));
+            assertThat(map, hasKey("insertRecords"));
             assertTrue(map.get("insertRecord") instanceof Record);
 
             Record insertRecord = (Record) map.get("insertRecord");
