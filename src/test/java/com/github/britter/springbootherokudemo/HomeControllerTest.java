163ad388214dc9b6b4537fc9ecc56cd51b0c567b
Benedikt Ritter <beneritter@gmail.com>
2016-01-09 13:17:38 +0100

    Add test coverage for insertData method

--- src/test/java/com/github/britter/springbootherokudemo/HomeControllerTest.java
+++ src/test/java/com/github/britter/springbootherokudemo/HomeControllerTest.java
@@ -17,14 +17,20 @@ package com.github.britter.springbootherokudemo;
 
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import de.bechte.junit.runners.context.HierarchicalContextRunner;
@@ -32,6 +38,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.MapBindingResult;
+import org.springframework.validation.ObjectError;
 
 @RunWith(HierarchicalContextRunner.class)
 public class HomeControllerTest {
@@ -83,4 +91,50 @@ public class HomeControllerTest {
         }
     }
 
+    public class InsertData {
+
+        private MapBindingResult bindingResult;
+
+        @Before
+        public void setUp() throws Exception {
+            bindingResult = new MapBindingResult(new HashMap<>(), "insertRecord");
+        }
+
+        @Test
+        public void shouldSaveRecordWhenThereAreNoErrors() throws Exception {
+            Record record = new Record();
+            insertData(record);
+
+            verify(repository, times(1)).save(record);
+        }
+
+        @Test
+        public void shouldNotSaveRecordWhenThereAreErrors() throws Exception {
+            bindingResult.addError(new ObjectError("", ""));
+
+            insertData(new Record());
+
+            verify(repository, never()).save(any(Record.class));
+        }
+
+        @Test
+        public void shouldAddNewInsertRecordToModelMap() throws Exception {
+            Record record = new Record();
+            insertData(record);
+
+            assertThat(map, hasKey("insertRecord"));
+            assertThat(map.get("insertRecord"), is(not(record)));
+        }
+
+        @Test
+        public void shouldAddRecordsToModelMap() throws Exception {
+            insertData(new Record());
+
+            assertThat(map, hasKey("records"));
+        }
+
+        private void insertData(Record record) {
+            ctrl.insertData(map, record, bindingResult);
+        }
+    }
 }
